14a8c1578948c1479e4b08851c50533f208c5f6b
masugerencias <yannpoirotsanz@gmail.com>
2022-07-24 07:37:04 +0200

    componente tarea completada y tachada

--- aplicacion-react-tareas/src/App.js
+++ aplicacion-react-tareas/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo1 from './imagenes/logo1.jpg'
+import Tarea from './componentes/Tarea';
 
 function App() {
   return (
@@ -11,10 +12,17 @@ function App() {
       </div>
       <div className='tareas-lista-principal'>
         <h1>Mis tareas</h1>
-        
+        <Tarea texto="esto es el prop joder!"/>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+/* 
+Entonces el prop se pone en el componente como si fuera un aparametrno.
+Para luego poner lo que es en el app.js donde se renderizxa ese componente 
+
+*/
--- aplicacion-react-tareas/src/componentes/Tarea.js
+++ aplicacion-react-tareas/src/componentes/Tarea.js
@@ -1,17 +1,22 @@
 import React from "react";
+import '../hojas-de-estilo/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tarea( {texto} ){
+function Tarea( {texto, completada} ){
     /* esto es un prop que se mete  como parametro
     y que dentro del return lo usaré */
     return(
-        <div className="tarea-contenedor">
+        <div className={completada ? 'tarea-contenedor completada': 'tarea-contenedor' }>
+            {/*         /* Si el valor del prop completada es TRUE entonces le asignamos dos clases separadas por un espacio : sino, pues que nos asigne la clase tarea-contenedor asecas.*/
+}
+            
             <div className="tarea-texto">
                 {texto}
                 {/* aqui hago uso del prop que he pasado en la funcion */}
                 
             </div>
-            <div className="tarea-icono">
-                icono de la X
+            <div className="tarea-contenedor-iconos">
+                <AiOutlineCloseCircle className="tarea-icono"/>
             </div>
 
         </div>
